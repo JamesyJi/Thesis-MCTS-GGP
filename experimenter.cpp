924eb70296a828142148499dc2857fb2f01aba1e
James Ji <jamesx.ji@gmail.com>
2022-05-10 19:50:45 +1000

    compiles i think

--- experimenter.cpp
+++ experimenter.cpp
@@ -16,10 +16,10 @@ int main(void)
 
     // auto manager = Main::GameManager<Models::Standard::Standard
 
-    auto m1 = Models::Standard::Standard<s>(Common::Player::PLAYER1, Connect4::Connect4State());
-    auto m2 = Models::Standard::Standard<s>(Common::Player::PLAYER2, Connect4::Connect4State());
+    // auto m1 = Models::Standard::Standard<s>(Common::Player::PLAYER1, Connect4::Connect4State());
+    // auto m2 = Models::Standard::Standard<s>(Common::Player::PLAYER2, Connect4::Connect4State());
 
-    auto m3 = Main::GameManager<Connect4::Connect4State, Connect4::Connect4Move>();
+    auto m3 = Main::GameManager<Models::Standard::Standard, Models::Standard::Standard, Connect4::Connect4State, Connect4::Connect4Move>();
 
 
     return 0;
--- main/game_manager.h
+++ main/game_manager.h
@@ -11,15 +11,13 @@
 namespace Main
 {
 
-template <typename TState, typename TMove>
+template <template <typename> class Model1, template <typename> class Model2, typename TState, typename TMove>
 class GameManager
 {
 public:
     using TraitsT = Models::Traits<TState, TMove>;
 
-    GameManager(Models::ModelType model1Type, Models::ModelType model2Type)
-    : mModel1Type(model1Type)
-    , mModel2Type(model2Type)
+    GameManager()
     {};
 
     ~GameManager(){};
@@ -29,8 +27,8 @@ public:
         for (int i = 0; i < rounds; ++i)
         {
             std::cout << "Starting round " << i << "\n";
-            auto model1 = Models::ModelFactory<TraitsT>::MakeModel(Common::Player::PLAYER1, mModel1Type);
-            auto model2 = Models::ModelFactory<TraitsT>::MakeModel(Common::Player::PLAYER2, mModel2Type);
+            auto model1 = Model1<TraitsT>(Common::Player::PLAYER1, TState());
+            auto model2 = Model2<TraitsT>(Common::Player::PLAYER2, TState());
 
             auto result = StartNewGame(model1, model2, resource);
 
@@ -39,7 +37,7 @@ public:
     }
 
 
-    Common::Result StartNewGame(Model1T& model1, Model2T& model2, Common::Resource& resource)
+    Common::Result StartNewGame(Model1<TraitsT>& model1, Model2<TraitsT> & model2, Common::Resource& resource)
     {
         TState state;
         TMove move;
@@ -60,19 +58,18 @@ public:
                     model1.GetChild(move);
                     break;
                 default:
-                    std::throw "No player's turn";
+                    throw std::runtime_error "No player's turn";
             }
 
             state.SimulateMove(move);
 
             std::cout << state;
+
+            playerTurn = Common::GetOtherPlayer(playerTurn);
         }
 
         return state.EvaluateState(move);
     }
-private:
-    Models::ModelType mModel1Type;
-    Models::ModelType mModel2Type;
 };
 
 }
