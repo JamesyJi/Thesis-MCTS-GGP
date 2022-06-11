4fe8cf44d49a25d3fbdf13804dfed85835c63b02
James Ji <jamesx.ji@gmail.com>
2022-06-11 17:20:57 +1000

    save

--- experimenter.cpp
+++ experimenter.cpp
@@ -13,13 +13,15 @@ int main(void)
     using MoveT = Connect4::Connect4Move;
     using StateT = Connect4::Connect4State;
     using GameT = Models::Traits<Connect4::Connect4State, Connect4::Connect4Move>;
-    using Model1T = Models::Minimax::MinimaxSelection<GameT, Strategy::FixedDepth4>;
-    using Model2T = Models::Minimax::MinimaxSelection<GameT, Strategy::DepthFromTurn<StateT>>;
+    // using Model1T = Models::Minimax::MinimaxSelection<GameT, Strategy::FixedDepth6>;
+    // using Model2T = Models::Minimax::MinimaxSelection<GameT, Strategy::FixedDepth5>;
+    using Model1T = Models::Standard::Standard<GameT>;
+    using Model2T = Models::Standard::Standard<GameT>;
 
     auto manager = Main::GameManager<Model1T, Model2T, StateT, MoveT>();
-    // auto resource = Common::IterationResource(100000);
-    auto resource = Common::TimeResource(0.3);
-    manager.StartExperiment(resource, 10);
+    auto resource = Common::IterationResource(100000);
+    // auto resource = Common::TimeResource(0.3);
+    manager.StartExperiment(resource, 5);
 
     return 0;
 }
