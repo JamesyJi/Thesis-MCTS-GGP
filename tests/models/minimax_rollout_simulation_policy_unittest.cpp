9e73b2226894d1fbf985f47bd8ae1ba0c9045930
James Ji <jamesx.ji@gmail.com>
2022-08-23 15:55:31 +1000

    some tests to give confidence

--- tests/models/minimax_rollout_simulation_policy_unittest.cpp
+++ tests/models/minimax_rollout_simulation_policy_unittest.cpp
@@ -3,7 +3,6 @@
 #include "pieces.h"
 #include "connect4_state.h"
 
-#include "model.h"
 #include "minimax/minimax_rollout.h"
 #include "strategy.h"
 
@@ -43,15 +42,57 @@ TEST_F(MinimaxRolloutSimulationPolicyUnitTest, AvoidsLosingMove_Depth2)
     };
     auto s = Connect4::Connect4State(position);
 
-    auto expectedMove1 = Connect4::Connect4Move(P2, 5, 2);
-    ASSERT_EQ(expectedMove1, m.SimulationPolicy(s, P2));
+    auto expectedMove = Connect4::Connect4Move(P2, 5, 2);
+    for (int i = 0; i < 20; ++i) ASSERT_EQ(expectedMove, m.SimulationPolicy(s, P2));    
+}
 
-    // s.SimulateMove(expectedMove1);
+TEST_F(MinimaxRolloutSimulationPolicyUnitTest, SelectsWinningSequence_Depth4)
+{
+    auto m = Models::Minimax::MinimaxRollout<Connect4GameT, Strategy::FixedDepth4>(mState, mGameState);
 
-    // MoveT expectedMove2 = ???
-    // ASSERT_EQ(expectedmove2, m.SimulationPolicy(s, P2));
+    const Common::Piece position[Connect4::Connect4State::ROWS][Connect4::Connect4State::COLS] = 
+    {
+        {A, A, A, A, A, A, A},
+        {A, A, A, A, A, A, A},
+        {A, A, A, A, A, A, A},
+        {A, A, A, A, A, A, A},
+        {A, A, A, A, O, O, A},
+        {A, A, A, A, X, X, A}
+    };
+    auto s = Connect4::Connect4State(position);
 
+    auto expectedMove1 = Connect4::Connect4Move(P1, 5, 3);
+    for (int i = 0; i < 20; ++i) ASSERT_EQ(expectedMove1, m.SimulationPolicy(s, P1));
 
+    s.SimulateMove(expectedMove1);
+    s.SimulateMove(Connect4::Connect4Move(P2, 5, 2));
+
+    auto expectedMove2 = Connect4::Connect4Move(P1, 5, 6);
+    for (int i = 0; i < 20; ++i) ASSERT_EQ(expectedMove2, m.SimulationPolicy(s, P1));
+}
+
+TEST_F(MinimaxRolloutSimulationPolicyUnitTest, DoesNotAvoidLosingMove_Depth3)
+{
+    auto m = Models::Minimax::MinimaxRollout<Connect4GameT, Strategy::FixedDepth2>(mState, mGameState);
+
+    const Common::Piece position[Connect4::Connect4State::ROWS][Connect4::Connect4State::COLS] = 
+    {
+        {A, A, A, A, A, A, A},
+        {A, A, A, A, A, A, A},
+        {A, A, A, A, A, A, A},
+        {A, A, A, A, A, A, A},
+        {A, A, A, A, O, A, A},
+        {A, A, A, A, X, X, A}
+    };
+    auto s = Connect4::Connect4State(position);
+
+    auto correctMove = Connect4::Connect4Move(P2, 5, 3);
+    int correctMoveCount = 0;
+    for (int i = 0; i < 50; ++i)
+    {
+        if (correctMove == m.SimulationPolicy(s, P2)) correctMoveCount++;
+    }
+    ASSERT_TRUE(correctMoveCount < 50);
 }
 
 }
--- tests/test_helpers/test_defines.h
+++ tests/test_helpers/test_defines.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#define P0 Common::Player::PLAYER0
 #define P1 Common::Player::PLAYER1
 #define P2 Common::Player::PLAYER2
 #define P1_WIN Common::Result::PLAYER1_WIN
