cb241b90dd6c0712ab1a815b90537e4feee971cd
James Ji <jamesx.ji@gmail.com>
2023-01-02 18:39:55 +1100

    removed ignored files

--- build/models/minimax/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/james/Documents/Thesis_C++/models/minimax
-# Build directory: /Users/james/Documents/Thesis_C++/build/models/minimax
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/models/standard/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/james/Documents/Thesis_C++/models/standard
-# Build directory: /Users/james/Documents/Thesis_C++/build/models/standard
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
